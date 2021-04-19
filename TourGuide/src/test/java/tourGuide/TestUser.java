75e7c35bc4062d511158a6a6190e692067395e6d
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-04-19 20:06:15 +0200

    Improve User unit test (userAddRewardTest)

--- TourGuide/src/test/java/tourGuide/TestUser.java
+++ TourGuide/src/test/java/tourGuide/TestUser.java
@@ -17,13 +17,17 @@ import java.util.UUID;
 public class TestUser {
 	
 	@Test
-	public void userAddRewardTest_shouldCorreclyAddReward() {
+	public void userAddRewardTest_shouldCorreclyAddRewardsWithoutDoublon() {
 		User user = new User(UUID.randomUUID(), "test", "0000", "test@test.com");
+		
 		Attraction attraction = new Attraction("Disneyland", "Anaheim", "CA", 33.817595D, -117.922008D);
 		VisitedLocation visitedLocation = new VisitedLocation(user.getUserId(), attraction, new Date());
-		user.addToVisitedLocations(visitedLocation);
 		UserReward reward = new UserReward(visitedLocation, attraction);
 		
+		Attraction attraction2 = new Attraction("Jackson Hole", "Jackson Hole", "WY", 43.582767D, -110.821999D);
+		VisitedLocation visitedLocation2 = new VisitedLocation(user.getUserId(), attraction2, new Date());
+		UserReward reward2 = new UserReward(visitedLocation2, attraction2);
+		
 		user.addUserReward(reward);
 		
 		assertEquals(1, user.getUserRewards().size());
@@ -31,6 +35,10 @@ public class TestUser {
 		user.addUserReward(reward);
 		
 		assertEquals(1, user.getUserRewards().size());
+		
+		user.addUserReward(reward2);
+		
+		assertEquals(2, user.getUserRewards().size());
 	}
 
 }
