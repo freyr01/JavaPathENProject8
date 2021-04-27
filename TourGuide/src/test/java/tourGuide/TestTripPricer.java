4b8c588e730231c4598418de8243306d5fa4a5fa
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-04-27 15:20:06 +0200

    Improve user preferences test

--- TourGuide/src/test/java/tourGuide/TestTripPricer.java
+++ TourGuide/src/test/java/tourGuide/TestTripPricer.java
@@ -1,8 +1,11 @@
 package tourGuide;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.javamoney.moneta.Money;
@@ -45,18 +48,24 @@ public class TestTripPricer {
 		prefs.setHighPricePoint(Money.of(BigDecimal.valueOf(100d), "EUR"));
 		prefs.setLowerPricePoint(Money.of(BigDecimal.valueOf(20.05), "EUR"));
 		prefs.setNumberOfAdults(2);
-		prefs.setNumberOfChildren(3);
+	
 		prefs.setTicketQuantity(5);
 		prefs.setTripDuration(14);
 		
 		User user = new User(UUID.randomUUID(), "test", "0000", "test@test.com");
 		user.setUserPreferences(prefs);
 		
-		List<Provider> providers = tripPricer.getPrice("", user.getUserId(), user.getUserPreferences().getNumberOfAdults(), 
-				user.getUserPreferences().getNumberOfChildren(), user.getUserPreferences().getTripDuration(), 10);
-		
-		for(Provider provider : providers) {
-			System.out.println(provider.name + ": " + provider.price);
+		for(int i = 0; i < 5; i++) {
+			prefs.setNumberOfChildren(i);
+			List<Provider> providers = tripPricer.getPrice("", user.getUserId(), user.getUserPreferences().getNumberOfAdults(), 
+					user.getUserPreferences().getNumberOfChildren(), user.getUserPreferences().getTripDuration(), 10);
+			
+			System.out.printf("Provider list with pref: %d children\n\n", prefs.getNumberOfChildren());
+			for(Provider provider : providers) {
+				System.out.println(provider.name + ": " + provider.price);
+
+			}
+			System.out.println();
 		}
 	}
 
