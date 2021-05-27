fadf14e9dbdcb8e337052a1fbbd7d79691b59b94
Matt <mathias.lauer.fr@gmail.com>
2021-05-27 19:37:24 +0200

    Rework build.gradle, add controller implementation and test

--- TourGuideGpsUtils/src/main/java/tourGuideGpsUtils/GpsUtilController.java
+++ TourGuideGpsUtils/src/main/java/tourGuideGpsUtils/GpsUtilController.java
@@ -1,5 +1,33 @@
 package tourGuideGpsUtils;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import gpsUtil.location.Attraction;
+import gpsUtil.location.VisitedLocation;
+
+@RestController
 public class GpsUtilController {
+	
+	
+	GpsUtilService gpsUtilService;
+	public GpsUtilController(@Autowired GpsUtilService p_gpsUtilService ) {
+		gpsUtilService = p_gpsUtilService;
+	}
+	
+	@GetMapping(value = "/getUserLocation")
+	public VisitedLocation getUserLocation(@RequestParam UUID userId) {
+		return gpsUtilService.getUserLocation(userId);
+	}
+	
+	@GetMapping(value="/getAttractions")
+	public List<Attraction> getAttractions(){
+		return gpsUtilService.getAttractions();
+	}
 
 }
--- TourGuideGpsUtils/src/test/java/TourGuideGpsUtils/GpsUtilTest.java
+++ TourGuideGpsUtils/src/test/java/TourGuideGpsUtils/GpsUtilTest.java
@@ -4,11 +4,34 @@
 package TourGuideGpsUtils;
 
 import org.junit.Test;
+
+import gpsUtil.location.VisitedLocation;
+import tourGuideGpsUtils.GpsUtilController;
+import tourGuideGpsUtils.GpsUtilService;
+
 import static org.junit.Assert.*;
 
+import java.util.UUID;
+
 public class GpsUtilTest {
-    @Test public void testSomeLibraryMethod() {
-        GpsUtilTest controller = new GpsUtilTest();
+	
+    @Test 
+    public void getAttractionsTest_shouldReturnAllRegisteredAttraction() {
+    	GpsUtilService service = new GpsUtilService();
+        GpsUtilController controller = new GpsUtilController(service);
         
+        
+        assertEquals(26, controller.getAttractions().size());
+        
+    }
+    
+    @Test
+    public void getUserLocationTest_shouldReturnUserLocation() {
+      	GpsUtilService service = new GpsUtilService();
+        GpsUtilController controller = new GpsUtilController(service);
+        
+        VisitedLocation visitedLocation = controller.getUserLocation(UUID.randomUUID());
+        
+        assertNotNull(visitedLocation.location);
     }
 }
