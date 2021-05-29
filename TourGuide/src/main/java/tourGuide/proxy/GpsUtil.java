4f0dc55abbbd4de2d872087988374d90e3cc473a
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-05-29 11:30:48 +0200

    Add getUserLocation proxy implementation

--- TourGuide/src/main/java/tourGuide/proxy/GpsUtil.java
+++ TourGuide/src/main/java/tourGuide/proxy/GpsUtil.java
@@ -2,11 +2,15 @@ package tourGuide.proxy;
 
 import java.util.List;
 
+import feign.Param;
 import feign.RequestLine;
 
 public interface GpsUtil {
 	
 	@RequestLine("GET /getAttractions")
 	public List<Attraction> getAttractions();
+	
+	@RequestLine("GET /getUserLocation?userId={userId}")
+	public VisitedLocation getUserLocation(@Param("userId") String userId);
 
 }
--- TourGuide/src/test/java/tourGuide/TestProxy.java
+++ TourGuide/src/test/java/tourGuide/TestProxy.java
@@ -1,11 +1,15 @@
 package tourGuide;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.UUID;
 
 import org.junit.Test;
 
 import feign.Feign;
 import feign.gson.GsonDecoder;
+import tourGuide.proxy.VisitedLocation;
 
 public class TestProxy {
 	
@@ -14,6 +18,14 @@ public class TestProxy {
 		tourGuide.proxy.GpsUtil gpsUtilProxy = Feign.builder().decoder(new GsonDecoder()).target(tourGuide.proxy.GpsUtil.class, "http://localhost:8081");
 		
 		assertEquals(26, gpsUtilProxy.getAttractions().size());
+	}
+	
+	@Test
+	public void getUtilProxyGetUserLocationTest_shouldReturnUserLocationUsingProxy() {
+		tourGuide.proxy.GpsUtil gpsUtilProxy = Feign.builder().decoder(new GsonDecoder()).target(tourGuide.proxy.GpsUtil.class, "http://localhost:8081");
+        VisitedLocation visitedLocation = gpsUtilProxy.getUserLocation(UUID.randomUUID().toString());
+        
+        assertNotNull(visitedLocation.location);
 	}
 
 }
