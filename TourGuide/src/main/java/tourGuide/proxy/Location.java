73a6ad1102c7c97b2397f4d45882dc0f1156f19b
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-05-29 10:55:13 +0200

    Create two proxy classes

---
+++ TourGuide/src/main/java/tourGuide/proxy/Location.java
@@ -0,0 +1,13 @@
+package tourGuide.proxy;
+
+public class Location {
+	
+    public final double longitude;
+    public final double latitude;
+
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+}
