6b22ba0b5318fb95231c5b2657c8c3d00982eeb0
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-05-31 14:40:42 +0200

    Add some Javadoc and log in gpsUtil microservice

--- TourGuideGpsUtils/src/main/java/tourGuideGpsUtils/GpsUtilController.java
+++ TourGuideGpsUtils/src/main/java/tourGuideGpsUtils/GpsUtilController.java
@@ -3,6 +3,8 @@ package tourGuideGpsUtils;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,19 +16,36 @@ import gpsUtil.location.VisitedLocation;
 @RestController
 public class GpsUtilController {
 	
+	private Logger logger = LoggerFactory.getLogger(GpsUtilController.class);
+
 	
-	GpsUtilService gpsUtilService;
+	private GpsUtilService gpsUtilService;
 	public GpsUtilController(@Autowired GpsUtilService p_gpsUtilService ) {
 		gpsUtilService = p_gpsUtilService;
 	}
 	
+	/**
+	 * Return an user location as Json
+	 * @param userId
+	 * @return VistedLocation
+	 * @author Mathias Lauer
+	 * 31 mai 2021
+	 */
 	@GetMapping(value = "/getUserLocation")
 	public VisitedLocation getUserLocation(@RequestParam UUID userId) {
+		logger.info("GET /getUserLocation with param: userId: {}", userId);
 		return gpsUtilService.getUserLocation(userId);
 	}
 	
+	/**
+	 * Return all attractions registered
+	 * @return List<Attraction>
+	 * @author Mathias Lauer
+	 * 31 mai 2021
+	 */
 	@GetMapping(value="/getAttractions")
 	public List<Attraction> getAttractions(){
+		logger.info("GET /getAttractions");
 		return gpsUtilService.getAttractions();
 	}
 
--- TourGuideGpsUtils/src/main/java/tourGuideGpsUtils/GpsUtilService.java
+++ TourGuideGpsUtils/src/main/java/tourGuideGpsUtils/GpsUtilService.java
@@ -13,10 +13,23 @@ public class GpsUtilService {
 	
 	private GpsUtil gpsUtil = new GpsUtil();
 	
+	/**
+	 * Get all attractions registered
+	 * @return List<Attraction>
+	 * @author Mathias Lauer
+	 * 31 mai 2021
+	 */
 	public List<Attraction> getAttractions() {
 		return gpsUtil.getAttractions();
 	}
 	
+	/**
+	 * Get an user last visited location
+	 * @param userId
+	 * @return VisitedLocation
+	 * @author Mathias Lauer
+	 * 31 mai 2021
+	 */
 	public VisitedLocation getUserLocation(UUID userId) {
 		return gpsUtil.getUserLocation(userId);
 	}
